fed362c819151b52c22d9110cd8800f653e08fb1
Damien Rembert <damien.rembert@gmail.com>
2026-03-17 23:45:23 +0000

    add setup script

---
+++ poor_man_stow.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# rename existing files to <original name>.old if not symlinks?
+
+ln -f .gitconfig ~/.gitconfig
+ln -f .bashrc ~/.bashrc
+ln -f .bash_aliases ~/.bash_aliases
+# ln -f .zsh_aliases ~/.zsh_aliases
+ln -f .zshrc ~/.zshrc
+ln -f .vimrc ~/.vimrc
+ln -f .tmux.conf ~/.tmux.conf
